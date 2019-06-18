5c2287ef2874147ceb5ee85facad83457b1a4f95
Ian Dailis <43609709+haharkagain@users.noreply.github.com>
2019-06-18 15:47:45 -0400

    movement and arena drawing works

---
+++ platformer.js
@@ -0,0 +1,99 @@
+window.addEventListener("load", platformer, false);
+function platformer() {
+    canv = document.getElementById("platformer");
+    ctx = canv.getContext("2d");
+    var unit = 40;
+    var posX = 0;
+    var posY = 0;
+    var velX = 5;
+    var left = false;
+    var up = false;
+    var right = false;
+    var down = false;
+    setInterval(function(){
+        move();
+    }, 1000/60)
+    
+    window.addEventListener("keyup", keyUp);
+    function keyUp(evt) {
+        if (evt.keyCode == 37) { // left
+            left = false;
+        }
+        if (evt.keyCode == 38) { // up
+            up = false;
+        }
+        if (evt.keyCode == 39) { // right
+            right = false;
+        }
+        if (evt.keyCode == 40) { // down
+            down = false;
+        }
+    }
+    window.addEventListener("keydown", keyDown);
+    function keyDown(evt) {
+        if (evt.keyCode == 37) { // left
+            left = true;
+        }
+        if (evt.keyCode == 38) { // up
+            up = true;
+        }
+        if (evt.keyCode == 39) { // right
+            right = true;
+        }
+        if (evt.keyCode == 40) { // down
+            down = true;
+        }
+    }
+
+    function move() {
+        if (left) {
+            posX -= velX;
+        }
+        if (up) {
+            posY--;
+        }
+        if (right) {
+            posX += velX;
+        }
+        if (down) {
+            posY++;
+        }
+    }
+    
+    arena = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    ]
+    function draw() {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canv.width, canv.height);
+        ctx.fillStyle = "lime";
+        ctx.fillRect(posX, posY, unit, unit);
+        ctx.fillStyle = "red";
+        for (y = 0; y < canv.height / unit; y++) {
+            for (x = 0; x < canv.width / unit; x++) {
+                if (arena[y][x] == 2) {
+                    ctx.fillRect(x * unit, y * unit, unit, unit);
+                }
+            }
+        }
+        window.requestAnimationFrame(draw);
+    }
+    window.requestAnimationFrame(draw);
+}
